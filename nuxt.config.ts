e173560c523511d67c632dc237293bddd1d49b68
Miyauchi Akira <miyauchi.akira@hey.com>
2025-05-31 20:31:46 +0900

    NuxtをGitHub Pagesに表示できるようにする

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,11 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  app: {
+    baseURL: '/mediamap-app/',
+  },
+  nitro: {
+    preset: 'static',
+  },
   devtools: { enabled: true },
   css: [
     '~/assets/css/base.css',
